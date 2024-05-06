44288921dd73faed2a01fbc24c2eacfa591be539
ChenWenDeng <cwd794873588@sina.com>
2024-05-06 12:38:56 +0800

    修改配置文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,11 @@
 module.exports = {
   title: 'Cwd-UI',
-  description: 'Just playing around',
   base: "/CwdUI/",
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Github', link: 'https://github.com/ChenWenDeng/CwdUI' },
-      { text: 'VuePress', link: 'https://vuepress.vuejs.org/' },
+      { text: 'Npm', link: 'https://www.npmjs.com/package/cwd-ui' },
     ],
     sidebar: ['/', '/componentDocs/card']
   }
